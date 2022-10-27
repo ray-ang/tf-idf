42d232bced6b44badf8c2db750fa606f3ea8fcd5
ray-ang <46805160+ray-ang@users.noreply.github.com>
2022-10-26 20:14:35 -0400

    Removed threshold

--- functions.php
+++ functions.php
@@ -1,100 +1,46 @@
 <?php
 
 /**
- * Text Preprocessing
- *
- * @param string $text  - text to preprocess
- * 
- * @return array        - processed tokenized terms
- */
-
-function preproc($text) {
-	if (! is_string($text)) return '$text parameter should be a string.';
-
-	$drop = ['!', '”', '#', '$', '%', '&', '’', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
-	foreach ($drop as $char) { // drop characters
-		$text = str_ireplace($char, '', $text);
-		$text = str_ireplace('   ', ' ', $text); // extra spaces
-		$text = str_ireplace('  ', ' ', $text); // extra space
-	}
-
-	$text = strtolower($text); // lowercase
-	$text = trim($text); // trim
-
-	$tokens = explode(' ', $text); // tokenization
-
-	foreach ($tokens as $key => $value) { // stemming
-		$arr = ['s' => -1, 'e' => -1, 'ly' => -2, 'ty' => -2, 'ed' => -2, 'er' => -2, 'ous' => -3, 'ful' => -3, 'ion' => -3, 'ing' => -3, 'ment' => -4];
-		foreach ($arr as $ky => $val) {
-			if ( substr($value, $val) == $ky && strlen(substr($value, 0, $val)) > 2 ) $tokens[$key] = substr($value, 0, $val);
-		}
-	}
-
-	return $tokens;
-}
-
-/**
- * TF-IDF using Term Documents with Threshold (TF-IDtF-T)
+ * Term Frequency–Inverse Document Frequency (TF-IDF)
  *
  * @param string $input      - document or input message
- * @param array $topic_terms - collection of topic terms
- * @param integer $threshold - term frequency threshold (raw)
+ * @param array $doc         - term document
  * 
- * @return array             - recommended subtopics
+ * @return array             - concepts by relevance
  */
 
-function tf_idtf_total($input, $topic_terms, $threshold) {
+function tf_idf_total($input, $doc) {
 	if (! is_string($input)) return '$input parameter should be a string.';
-	if (! is_array($topic_terms)) return '$topic_terms parameter should be an array.';
-	if (! is_integer($threshold) || $threshold < 0) return '$threshold parameter should be a positive integer.';
+	if (! is_array($doc)) return '$doc parameter should be an array.';
 
-	$input_tokens = preproc($input); // preprocess input
-
-	$term_tokens = array();
-	foreach ($topic_terms as $key => $value) { // preprocess topic terms
-		$term_tokens[$key] = preproc($value);
-	}
-
-	$num_tokens = count($input_tokens);
-	$total_subtopics = count($term_tokens);
-	$distinct_tokens = array_unique($input_tokens);
-	$num_distinct_tokens = count($distinct_tokens);
+	$input = trim(strtolower($input));
+	$doc1 = array();
 	
-	$threshold = $threshold / $num_distinct_tokens; // normalized threshold
+	foreach ($doc as $key => $val) {
+		$count = 0;
+		$tf_idf = 0;
 
-	$tf_array = array();
-	foreach ($term_tokens as $key => $value) {
-		$tf_n = 0;
-		$tf_idtf = 0;
-		foreach ($value as $term) {
-			$tf_raw = 0;
-			if (in_array($term, $input_tokens)) { // raw TF
-				$tf_raw = array_count_values($input_tokens)[$term];
-			}
+		foreach ($val as $row) {
+			$row = strtolower($row);
+			$freq = substr_count($input, $row); // raw frequency
+			$count = $freq / count( explode(' ', $input) ); // normalized frequency
 
-			$num_subtopics = 0;
-			foreach ($term_tokens as $ky => $val) {
-				if (in_array($term, $val)) {
-					$num_subtopics += 1;
+			$in_doc = 0;
+			foreach ($doc as $ky => $vl) {
+				if (in_array($row, $vl)) {
+					$in_doc += 1;
 				}
 			}
 
-			$tf_n += $tf_raw / $num_tokens; // Total normalized TF
-			$tf_idtf += ($tf_raw / $num_tokens) * (log10($total_subtopics / $num_subtopics)); // Total TF-IDtF
+			$tf_idf += $count * (log10(count($doc) / $in_doc)); // sum TF-IDF
 		}
-
-		$input_count = 0;
-		foreach ($distinct_tokens as $input) { // count distinct input in term document
-			if (in_array($input, $value)) {
-				$input_count += 1;
-			}
-		}
-
-		if ( ($input_count/$num_distinct_tokens) >= $threshold ) { // signal detection
-			$tf_array[$key] = $tf_idtf; // recommended topics
-		}
+		
+		$doc1[$key] = $tf_idf;
 	}
 
-	arsort($tf_array); // sort values descending
-	return $tf_array;
+	unset($doc);
+	gc_collect_cycles();
+
+	arsort($doc1);
+	return $doc1;
 }
--- index.php
+++ index.php
@@ -3,17 +3,16 @@
 require __DIR__ . '/functions.php';
 
 $pain = array(); // term collections document
-$pain['presence'] = 'pain hurt ache discomfort in have experiencing'; // term collection
-$pain['location'] = 'pain hurt ache discomfort where at location area point show part';
-$pain['time'] = 'pain hurt ache discomfort when long started now';
-$pain['degree'] = 'pain hurt ache discomfort rate scale degree how bad better worse';
-$pain['spread'] = 'pain hurt ache discomfort spread radiate area';
+$pain['presence'] = ['pain', 'hurt', 'ach', 'discomfort', 'hav', 'experienc']; // term collection
+$pain['location'] = ['pain', 'hurt', 'ach', 'discomfort', 'where', 'locat', 'area', 'point', 'show', ' part'];
+$pain['time'] = ['pain', 'hurt', 'ach', 'discomfort', 'when', 'how long', 'often', 'start', 'begin', 'time', 'now', ' day'];
+$pain['degree'] = ['pain', 'hurt', 'ach', 'discomfort', 'rate', 'scale', 'degree', 'how bad', 'better', 'wors'];
+$pain['spread'] = ['pain', 'hurt', 'ach', 'discomfort', 'spread', 'radiat', 'area'];
 
 $message1 = 'Are you in pain? If so, which part of your body are you having pain?'; // message
 $message2 = 'How bad is your pain? Can you point where the pain is, and does the pain spread to other areas?';
 $message3 = 'Where is the pain? Can you show me where it hurts?';
 
-// Threshold adjustment
-var_dump( tf_idtf_total($message1, $pain, 2) ); // presence and location
-var_dump( tf_idtf_total($message2, $pain, 2) ); // degree, location and spread
-var_dump( tf_idtf_total($message3, $pain, 3) ); // location
+var_dump( tf_idf_total($message1, $pain) ); // presence and location
+// var_dump( tf_idf_total($message2, $pain) ); // location, spread and degree
+// var_dump( tf_idf_total($message3, $pain) ); // location
